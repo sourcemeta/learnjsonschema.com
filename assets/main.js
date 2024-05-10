bdef865b647b48206e6711eacd0a38e03cf6b9a8
Suprith KG <suprith7kg@gmail.com>
2024-05-10 21:05:23 +0530

    Dropdown added too the breadcrumb

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-import './vendor/bootstrap/js/src/dropdown.js';
-import Tooltip from './vendor/bootstrap/js/src/tooltip.js';
-
-// Initialize tooltips
-for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
-  new Tooltip(element);
-}
