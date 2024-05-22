9667beb53fa4936549dc882f38eb6953387a56fd
Suprith KG <suprith7kg@gmail.com>
2024-05-23 00:40:13 +0530

    Changed toggle bar to pills, using JS

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,50 @@
 import './vendor/bootstrap/js/src/dropdown.js';
 import Tooltip from './vendor/bootstrap/js/src/tooltip.js';
 
-// Initialize tooltips
-for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
-  new Tooltip(element);
+const vocabularyTab = document.getElementById('vocabulary-tab');
+const alphabeticTab = document.getElementById('alphabetic-tab');
+const vocabularyContent = document.getElementById('vocabulary');
+const alphabeticContent = document.getElementById('alphabetic');
+
+let alphabeticOrder = sessionStorage.getItem('alphabeticOrder');
+
+if (alphabeticOrder === 'true') { 
+  vocabularyTab.classList.remove('active');
+  alphabeticTab.classList.add('active');
+  alphabeticContent.classList.add('show', 'active');
+  alphabeticContent.classList.remove('d-none');
+} else {
+  alphabeticTab.classList.remove('active');
+  vocabularyTab.classList.add('active');
+  vocabularyContent.classList.add('show', 'active');
+  vocabularyContent.classList.remove('d-none')
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize tooltips
+  for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
+    new Tooltip(element);
+  }
+
+  const vocabularyTab = document.getElementById('vocabulary-tab');
+  const alphabeticTab = document.getElementById('alphabetic-tab');
+  const vocabularyContent = document.getElementById('vocabulary');
+  const alphabeticContent = document.getElementById('alphabetic');
+
+  vocabularyTab.addEventListener('click', function () {
+    sessionStorage.setItem('alphabeticOrder', 'false'); // Store as string
+    vocabularyTab.classList.add('active');
+    alphabeticTab.classList.remove('active');
+    vocabularyContent.classList.add('show', 'active');
+    vocabularyContent.classList.remove('d-none')
+  });
+
+  alphabeticTab.addEventListener('click', function () {
+    sessionStorage.setItem('alphabeticOrder', 'true'); // Store as string
+    alphabeticTab.classList.add('active');
+    vocabularyTab.classList.remove('active');
+    vocabularyContent.classList.add('d-none');
+    alphabeticContent.classList.remove('d-none');
+    alphabeticContent.classList.add('show', 'active');
+  });
+});
