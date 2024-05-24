fb764d9b76f4508ea11690222ecc0f77d285a05b
Suprith KG <suprith7kg@gmail.com>
2024-05-24 18:41:56 +0530

    Remove DOMContentLoaded listenr

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,10 @@
 import './vendor/bootstrap/js/src/dropdown.js';
 import Tooltip from './vendor/bootstrap/js/src/tooltip.js';
 
+for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
+  new Tooltip(element);
+}
+
 const vocabularyTab = document.getElementById('vocabulary-tab');
 const alphabeticTab = document.getElementById('alphabetic-tab');
 const vocabularyContent = document.getElementById('vocabulary');
@@ -20,31 +24,20 @@ if (alphabeticOrder === 'true') {
   vocabularyContent.classList.remove('d-none')
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Initialize tooltips
-  for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
-    new Tooltip(element);
-  }
 
-  const vocabularyTab = document.getElementById('vocabulary-tab');
-  const alphabeticTab = document.getElementById('alphabetic-tab');
-  const vocabularyContent = document.getElementById('vocabulary');
-  const alphabeticContent = document.getElementById('alphabetic');
+vocabularyTab.addEventListener('click', function () {
+  sessionStorage.setItem('alphabeticOrder', 'false'); // Store as string
+  vocabularyTab.classList.add('active');
+  alphabeticTab.classList.remove('active');
+  vocabularyContent.classList.add('show', 'active');
+  vocabularyContent.classList.remove('d-none')
+});
 
-  vocabularyTab.addEventListener('click', function () {
-    sessionStorage.setItem('alphabeticOrder', 'false'); // Store as string
-    vocabularyTab.classList.add('active');
-    alphabeticTab.classList.remove('active');
-    vocabularyContent.classList.add('show', 'active');
-    vocabularyContent.classList.remove('d-none')
-  });
-
-  alphabeticTab.addEventListener('click', function () {
-    sessionStorage.setItem('alphabeticOrder', 'true'); // Store as string
-    alphabeticTab.classList.add('active');
-    vocabularyTab.classList.remove('active');
-    vocabularyContent.classList.add('d-none');
-    alphabeticContent.classList.remove('d-none');
-    alphabeticContent.classList.add('show', 'active');
-  });
+alphabeticTab.addEventListener('click', function () {
+  sessionStorage.setItem('alphabeticOrder', 'true'); // Store as string
+  alphabeticTab.classList.add('active');
+  vocabularyTab.classList.remove('active');
+  vocabularyContent.classList.add('d-none');
+  alphabeticContent.classList.remove('d-none');
+  alphabeticContent.classList.add('show', 'active');
 });
