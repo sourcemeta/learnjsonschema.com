a5bb47b9ce45127c675a728943bd8b0783dc50a1
Suprith KG <suprith7kg@gmail.com>
2024-06-22 04:47:19 +0530

    Extend the sidebar to see keywords by either vocabulary or alphabetically (#254)

--- assets/main.js
+++ assets/main.js
@@ -31,3 +31,39 @@ function copyToClipboard(text, button) {
       console.error("Failed to copy text: ", err);
     });
 }
+
+const vocabularyTab = document.getElementById('sidebar-grouping-vocabulary');
+const alphabeticTab = document.getElementById('sidebar-grouping-alphabetic');
+const vocabularyContent = document.getElementById('vocabulary');
+const alphabeticContent = document.getElementById('alphabetic');
+
+function setSidebarGrouping(sidebarGrouping) {
+  if (sidebarGrouping === 'alphabetic') {
+    vocabularyTab.classList.remove('active');
+    alphabeticTab.classList.add('active');
+    vocabularyContent.classList.remove('show', 'active');
+    vocabularyContent.classList.add('d-none');
+    alphabeticContent.classList.add('show', 'active');
+    alphabeticContent.classList.remove('d-none');
+  } else {
+    alphabeticTab.classList.remove('active');
+    vocabularyTab.classList.add('active');
+    alphabeticContent.classList.remove('show', 'active');
+    alphabeticContent.classList.add('d-none');
+    vocabularyContent.classList.add('show', 'active');
+    vocabularyContent.classList.remove('d-none');
+  }
+}
+
+const sidebarGrouping = localStorage.getItem('sidebarGrouping');
+setSidebarGrouping(sidebarGrouping);
+
+vocabularyTab.addEventListener('click', function () {
+  localStorage.setItem('sidebarGrouping', 'vocabulary');
+  setSidebarGrouping('vocabulary');
+});
+
+alphabeticTab.addEventListener('click', function () {
+  localStorage.setItem('sidebarGrouping', 'alphabetic');
+  setSidebarGrouping('alphabetic');
+});
