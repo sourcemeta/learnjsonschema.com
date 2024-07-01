26f87cf65b7b83075b59cebf0c9c54a86af50ddb
Suprith KG <suprith7kg@gmail.com>
2024-07-01 13:39:17 +0530

    Add a search box on the navigation bar (#255)

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,48 @@
 import './vendor/bootstrap/js/src/dropdown.js';
 import Tooltip from './vendor/bootstrap/js/src/tooltip.js';
+import Fuse from 'fuse.js';
 
-// Initialize tooltips
-for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
-  new Tooltip(element);
+
+const executeSearch = (searchQuery) => {
+  // TODO - loading screen
+
+  fetch('/index.json').then((response) => {
+      if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+      }
+      response.json().then((pages) => {
+        var fuse = new Fuse(pages, fuseOptions);
+        var result = fuse.search(searchQuery);
+        updateDropdown(result.slice(0, 10));
+      }).catch((err) => {
+        console.error('Fetch Index Fetch Error :-S', err);
+      });
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".copy-btn").forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const codeBlock = btn.nextElementSibling;
-      if (codeBlock && codeBlock.textContent) {
-        copyToClipboard(codeBlock.textContent, btn);
-      }
-    });
-  });
-});
+const updateDropdown = (results) => {
+  // Clear previous results
+  searchResultBox.innerHTML = '';
 
-function copyToClipboard(text, button) {
+  if (results.length > 0) {
+    results.forEach((result) => {
+      const item = document.createElement('li');
+      item.innerHTML = `<a class="dropdown-item" href="${result.item.permalink}">
+        ${result.item.title}
+        <span class="text-light-50" style="font-size: 0.8em;">(${result.item.dialect})</span>
+        <span class="fw-bold" style="font-size: 0.9em;">${result.item.vocabulary}</span>
+      </a>`;
+      searchResultBox.appendChild(item);
+    });
+  } else {
+    const item = document.createElement('li');
+    item.innerHTML = '<a class="dropdown-item disabled" href="#">No matches found</a>';
+    searchResultBox.appendChild(item);
+  }  
+}
+
+const copyToClipboard = (text, button) => {
   navigator.clipboard
     .writeText(text)
     .then(() => {
@@ -31,6 +56,56 @@ function copyToClipboard(text, button) {
       console.error("Failed to copy text: ", err);
     });
 }
+
+// Initialize tooltips
+for (const element of document.querySelectorAll('[data-bs-toggle="tooltip"]')) {
+  new Tooltip(element);
+}
+
+// Fuse search options
+const fuseOptions = {
+  shouldSort: true,
+  includeMatches: true,
+  includeScore: true,
+  keys: [
+    { name: "title", weight: 0.45 },
+    { name: "contents", weight: 0.4 },
+    { name: "tags", weight: 0.1 },
+    { name: "categories", weight: 0.05 }
+  ]
+};
+
+const searchInputContent = document.getElementById('search-content');
+const searchResultBox = document.getElementById('search-result-list');
+
+searchInputContent.addEventListener('input', () => {
+  const query = searchInputContent.value;
+
+  if (query) {
+    executeSearch(query);
+    searchResultBox.style.display = 'block';
+  } else {
+    searchResultBox.style.display = 'none';
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (!searchInputContent.contains(event.target) && !searchResultBox.contains(event.target)) {
+    searchResultBox.style.display = 'none';
+    searchInputContent.value = ''; // Clear the search input
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".copy-btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const codeBlock = btn.nextElementSibling;
+      if (codeBlock && codeBlock.textContent) {
+        copyToClipboard(codeBlock.textContent, btn);
+      }
+    });
+  });
+});
 
 const vocabularyTab = document.getElementById('sidebar-grouping-vocabulary');
 const alphabeticTab = document.getElementById('sidebar-grouping-alphabetic');
